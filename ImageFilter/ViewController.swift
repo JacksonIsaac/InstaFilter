84a1058da7afa045ffc6ee97b5617919e8c2142e
Jackson Isaac <jacksonisaac@Jacksons-MacBook-Pro.local>
2016-11-05 01:10:47 +0530

    Initial Commit

---
+++ ImageFilter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageFilter
+//
+//  Created by Jackson Isaac on 05/11/16.
+//  Copyright © 2016 Jackson Isaac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
