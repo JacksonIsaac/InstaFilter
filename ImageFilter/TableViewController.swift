3ee68bbebb1250ad43100acce7dcc9bcf2625062
Jackson Isaac <jacksonisaac2008@gmail.com>
2016-11-08 16:07:28 +0530

    Add support for importing images from Camera or Photo Library.

--- ImageFilter/TableViewController.swift
+++ ImageFilter/TableViewController.swift
@@ -8,30 +8,46 @@
 
 import UIKit
 
-class TableViewController: UIViewController {
+class TableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    /*
+     * Table View is not set in the beginning as the content may change.
+     * Its data need to be able to reload and seperate from its presentation.
+     * Delegate: Delegate actions to the table. (handle actions and UI controls)
+     * Data Source: Delegate control of the data to the table. (provide data)
+     * Protocols are added for Delegate
+     */
     @IBOutlet var tableView: UITableView!
+    
+    let filters = [
+        "Red",
+        "Blue",
+        "Green",
+        "Yellow",
+        "Purple",
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(filters[indexPath.row])
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filters.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FilterCell", for: indexPath)
+        
+        cell.textLabel?.text = filters[indexPath.row]
+        
+        return cell
+    }
 }
