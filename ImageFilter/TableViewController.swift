91521bbe655de2088ff2df668e024f7d7f13a0f4
Jackson Isaac <jacksonisaac2008@gmail.com>
2016-11-08 13:17:34 +0530

    Create TableViewController Class. Attach Table view to the new TableViewController Class.

---
+++ ImageFilter/TableViewController.swift
@@ -0,0 +1,37 @@
+//
+//  TableViewController.swift
+//  ImageFilter
+//
+//  Created by Jackson Isaac on 08/11/16.
+//  Copyright © 2016 Jackson Isaac. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UIViewController {
+
+    @IBOutlet var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
