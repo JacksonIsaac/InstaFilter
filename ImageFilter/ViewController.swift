c4ec1d29af8ec9ae859b7a771eab8a40a962dc15
Jackson Isaac <jacksonisaac2008@gmail.com>
2016-11-08 13:03:22 +0530

    Add fade animation to secondary menu

--- ImageFilter/ViewController.swift
+++ ImageFilter/ViewController.swift
@@ -23,6 +23,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        secondaryMenu.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+    
+        secondaryMenu.translatesAutoresizingMaskIntoConstraints = false
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,8 +35,45 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func onFilter(_ sender: Any) {
+    @IBAction func onFilter(_ sender: UIButton) {
+        if(sender.isSelected) {
+            hideSecondaryMenu()
+            sender.isSelected = false
+        } else {
+            showSecondaryMenu()
+            sender.isSelected = true
+        }
+    }
+    
+    func showSecondaryMenu() {
         view.addSubview(secondaryMenu)
+        
+        let bottomConstraint = secondaryMenu.bottomAnchor.constraint(equalTo: bottomMenu.topAnchor)
+        let leftConstraint = secondaryMenu.leftAnchor.constraint(equalTo: view.leftAnchor)
+        let rightConstraint = secondaryMenu.rightAnchor.constraint(equalTo: view.rightAnchor)
+        
+        let heightConstraint = secondaryMenu.heightAnchor.constraint(equalToConstant: 44)
+        
+        NSLayoutConstraint.activate([bottomConstraint, leftConstraint, rightConstraint, heightConstraint])
+        
+        view.layoutIfNeeded()
+        
+        self.secondaryMenu.alpha = 0
+
+        UIView.animate(withDuration: 0.4) {
+            self.secondaryMenu.alpha = 1.0
+        }
+        
+    }
+    
+    func hideSecondaryMenu() {
+        UIView.animate(withDuration: 0.4, animations: {
+            self.secondaryMenu.alpha = 0
+        }, completion: { completed in
+            if completed == true {
+                self.secondaryMenu.removeFromSuperview()
+            }
+        })
     }
 
 }
